65816843d7b3143657dcdae3a39384d8c4e48b2e
Dok6n <ehrbs2018@gmail.com>
2022-12-04 14:53:08 +0900

    react-router-dom 튜토리얼 예제 추가

---
+++ packages/react-router-tutorial-v6/src/ErrorPage.tsx
@@ -0,0 +1,28 @@
+/**
+ * 에러 페이지
+ */
+
+import { useRouteError } from 'react-router-dom';
+
+interface IErrorResponse {
+  status: number;
+  statusText: string;
+  internal: boolean;
+  data: string;
+  error: { message: string; stack: string };
+}
+
+export default function ErrorPage() {
+  const error = useRouteError() as IErrorResponse;
+  console.error(error);
+
+  return (
+    <div id='error-page'>
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText}</i>
+      </p>
+    </div>
+  );
+}
